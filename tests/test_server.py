5af1c307a0c376395692e62ce5053a48a1d8b16d
bibi21000 <bibi21000@gmail.com>
2016-02-20 19:05:02 +0100

    Updates tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -70,5 +70,5 @@ class TestHostSensorSerser(JNTTServer, JNTTServerCommon):
             time.sleep(120)
         finally:
             self.stop()
-        self.assertInLogfile('Found heartbeats in timeout')
         self.assertNotInLogfile('^ERROR ')
+        self.assertInLogfile('Found heartbeats in timeout')
