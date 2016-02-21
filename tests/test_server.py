2f8df0faa10a5e4a8008d6570dddfeaec526af5c
bibi21000 <bibi21000@gmail.com>
2016-02-21 23:27:14 +0100

    Update tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -45,17 +45,6 @@ from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_S
 
 from janitoo_hostsensor.server import HostSensorServer
 
-##############################################################
-#Check that we are in sync with the official command classes
-#Must be implemented for non-regression
-from janitoo.classes import COMMAND_DESC
-
-COMMAND_DISCOVERY = 0x5000
-
-assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
-##############################################################
-
-
 class TestHostSensorSerser(JNTTServer, JNTTServerCommon):
     """Test the hostsensor server
     """
